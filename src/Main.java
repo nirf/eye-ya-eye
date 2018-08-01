15e5f8827a01cc21e2c62b9bb0114215a00102ff
nirfagi <P0lly782>
2018-08-01 17:48:52 +0300

    extract search term parsing to a function

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,12 @@ public class Main {
 
         List<List<String>> list = CommaSeparatedValuesFilesUtils.getMatrix(searchTermFilePaht);
 
+        SearchTermAggregation searchTermAggregation = CommaSeparatedValuesFilesUtils.parseSearchTermAggregation(list);
+
         for (int i = 0; i < 6; i++) {
             System.out.println(list.get(i));
         }
 
-        SearchTermAggregation searchTermAggregation = new SearchTermAggregation();
-
-        searchTermAggregation.setExportDate(list.get(0).get(0).replace("|", ",").split(",")[0].replace("Export date: ", "").trim().replace("\"", ""));
-        searchTermAggregation.setExportTime(list.get(0).get(0).replace("|", ",").split(",")[1].replace("Export time: ", "").trim().replace("\"", ""));
-
-        searchTermAggregation.setSearchTerm(list.get(1).get(0).substring(list.get(1).get(0).indexOf(":") + 1).trim().replace("\"", ""));
-
-        searchTermAggregation.setAverageSales(Float.valueOf(list.get(2).get(0).substring(list.get(2).get(0).indexOf(":") + 1, list.get(2).get(0).length() - 1).trim().replace("\"", "")));
-
-        String averageSalesRank = new StringBuilder(list.get(3).get(0).substring(list.get(3).get(0).indexOf(":") + 1, list.get(3).get(0).length()).trim().replace("\"", ""))
-//                .append(",")
-                .append(list.get(3).get(1).trim().replace("\"", ""))
-                .toString();
-
-        System.out.println(averageSalesRank);
-        searchTermAggregation.setAverageSalesRank(Float.valueOf(averageSalesRank));
-
-        searchTermAggregation.setAveragePrice(Float.valueOf(list.get(4).get(0).substring(list.get(4).get(0).indexOf("$") + 1, list.get(4).get(0).length() - 1).trim().replace("\"", "")));
-
-        searchTermAggregation.setAverageReviews(Float.valueOf(list.get(5).get(0).substring(list.get(5).get(0).indexOf(":") + 1, list.get(5).get(0).length() - 1).trim().replace("\"", "")));
 
         System.out.println(searchTermAggregation);
 
--- src/utils/files/CommaSeparatedValuesFilesUtils.java
+++ src/utils/files/CommaSeparatedValuesFilesUtils.java
@@ -1,5 +1,7 @@
 package utils.files;
 
+import models.SearchTermAggregation;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -37,5 +39,27 @@ public class CommaSeparatedValuesFilesUtils {
             }
         }
         return matrix;
+    }
+
+    public static SearchTermAggregation parseSearchTermAggregation(List<List<String>> list) {
+        SearchTermAggregation searchTermAggregation = new SearchTermAggregation();
+        searchTermAggregation.setExportDate(list.get(0).get(0).replace("|", ",").split(",")[0].replace("Export date: ", "").trim().replace("\"", ""));
+        searchTermAggregation.setExportTime(list.get(0).get(0).replace("|", ",").split(",")[1].replace("Export time: ", "").trim().replace("\"", ""));
+
+        searchTermAggregation.setSearchTerm(list.get(1).get(0).substring(list.get(1).get(0).indexOf(":") + 1).trim().replace("\"", ""));
+
+        searchTermAggregation.setAverageSales(Float.valueOf(list.get(2).get(0).substring(list.get(2).get(0).indexOf(":") + 1, list.get(2).get(0).length() - 1).trim().replace("\"", "")));
+
+        String averageSalesRank = new StringBuilder(list.get(3).get(0).substring(list.get(3).get(0).indexOf(":") + 1, list.get(3).get(0).length()).trim().replace("\"", ""))
+                .append(list.get(3).get(1).trim().replace("\"", ""))
+                .toString();
+
+        searchTermAggregation.setAverageSalesRank(Float.valueOf(averageSalesRank));
+
+        searchTermAggregation.setAveragePrice(Float.valueOf(list.get(4).get(0).substring(list.get(4).get(0).indexOf("$") + 1, list.get(4).get(0).length() - 1).trim().replace("\"", "")));
+
+        searchTermAggregation.setAverageReviews(Float.valueOf(list.get(5).get(0).substring(list.get(5).get(0).indexOf(":") + 1, list.get(5).get(0).length() - 1).trim().replace("\"", "")));
+
+        return searchTermAggregation;
     }
 }
